a970c6a392713fbcb2353a12bbb001e7355683de
dariocast <dariocastellano18@gmail.com>
2017-01-25 10:53:28 +0100

    add component SPODTUTORIAL_CMP_Detail in file detail.php and it's view detail.html. This component show an hint relative to how to pass a challenge

---
+++ components/detail.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: darcas
+ * Date: 24/01/2017
+ * Time: 15:48
+ */
+class SPODTUTORIAL_CMP_Detail extends OW_Component
+{
+    public function __construct($params)
+    {
+        $userId = (int) $params['userId'];
+        $passedChallengesId = json_decode(SPODTUTORIAL_BOL_ProgressService::getInstance()->findByUserId($userId)->passedChallengesId);
+        $challenges = SPODTUTORIAL_BOL_ChallengeDao::getInstance()->findByIdList($passedChallengesId);
+
+        $this->assign('components_url', SPODPR_COMPONENTS_URL);
+        $this->assign('challenges',$challenges);
+        $this->assign('prefix','spodtutorial+');
+    }
+}
