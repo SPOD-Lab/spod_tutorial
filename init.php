435bdc93280b34d20f989c49f1a23da424c04aad
dariocast <dariocastellano18@gmail.com>
2017-02-13 16:13:14 +0100

    fixed error while trying installation without some required plugin

--- init.php
+++ init.php
@@ -9,4 +9,10 @@ $preference = BOL_PreferenceService::getInstance()->findPreference('spodpr_compo
 $spodpr_components_url = empty($preference) ? "http://deep.routetopa.eu/deep-components/" : $preference->defaultValue;
 define("SPOD_COMPONENTS_URL", $spodpr_components_url);
 
+if(OW::getPluginManager()->isPluginActive('ode') &&
+    OW::getPluginManager()->isPluginActive('spodpublic') &&
+    OW::getPluginManager()->isPluginActive('spodpr') &&
+    OW::getPluginManager()->isPluginActive('cocreation') &&
+    OW::getPluginManager()->isPluginActive('newsfeed')
+)
 SPODTUTORIAL_BOL_ProgressService::getInstance()->initDb();
